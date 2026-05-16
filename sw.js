37b15b4bd2943ca55c6c9cb8f6b8a3b92d49ed57
DevWagnerEmerich <58908343+DevWagnerEmerich@users.noreply.github.com>
2026-05-16 00:00:56 -0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -18,8 +18,15 @@ self.addEventListener('install', (event) => {
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
+    fetch(event.request)
+      .then((networkResponse) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, networkResponse.clone());
+          return networkResponse;
+        });
+      })
+      .catch(() => {
+        return caches.match(event.request);
+      })
   );
 });
